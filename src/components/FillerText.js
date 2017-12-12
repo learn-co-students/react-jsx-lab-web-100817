2424e2f8e1a4b47b40da9dab7d6daf3306727a95
Alona <github email address>
2017-12-12 15:46:19 -0500

    Done.

---
+++ src/components/FillerText.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+//
+// export default class FillerText extends React.Component {
+//   render() {
+//     return (
+//       <p>
+//         I am a filler text. I can be used to fill your screen. Amazing!
+//       </p>
+//     )
+//   }
+// }
+
+import React from 'react';
+
+export default class FillerText extends React.Component {
+  render() {
+    return (
+      <p>
+      I am filler text. I can be used to fill your screen. Amazing
+      </p>
+    )
+  }
+}
