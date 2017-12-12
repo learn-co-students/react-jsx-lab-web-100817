f3aa472b35a95db35148e598d6f7d9fcf526cc2a
KingGranger <cheeridon@gmail.com>
2017-12-12 13:19:55 -0500

    Done.

---
+++ src/components/FillerText.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import ReactDom from 'react-dom';
+
+export default class FilterText extends React.Component{
+  render(){
+    return (
+      <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
+    )
+  }
+}
